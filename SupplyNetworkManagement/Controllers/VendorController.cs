1acfe151ef3d612bdd3ef69b2bf29b4a82821809
DavidT955 <dtaylor0715@conestogac.on.ca>
2026-03-31 20:51:10 -0400

    Added MyDBContext (SQLite) and vendor controller

---
+++ SupplyNetworkManagement/Controllers/VendorController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SupplyNetworkManagement.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SupplyNetworkManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+        private readonly MyDbContext m_db;
+        public VendorController(MyDbContext db)
+        {
+            m_db = db;
+        }
+
+        // GET: api/Vendor
+        [HttpGet]
+        public ActionResult<IEnumerable<Vendor>> Get()
+        {
+            
+            var vendors = m_db.Vendors.ToList();
+            return Ok(vendors);
+        }
+
+        // GET api/Vendor/id#
+        [HttpGet("{id}")]
+        public ActionResult<Vendor> Get(int id)
+        {
+            var v = m_db.Vendors.Where(v => v.VendorId == id).SingleOrDefault();
+            return Ok(v);
+        }
+
+        // POST api/Vendor
+        [HttpPost]
+        public void Post([FromBody]Vendor v)
+        {
+        
+            m_db.Vendors.Add(v);
+            m_db.SaveChanges();
+        }
+
+        // PUT api/Vendor/
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/Vendor/
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- SupplyNetworkManagement/Program.cs
+++ SupplyNetworkManagement/Program.cs
@@ -1,12 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using SupplyNetworkManagement.Data; // 1. Add this namespace
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// 2. Register your DbContext with SQLite
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseSqlite(connectionString));
+
 var app = builder.Build();
+
+// 3. Automatically create/update the database on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,13 +28,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseDefaultFiles();  
-app.UseStaticFiles();   
-
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
